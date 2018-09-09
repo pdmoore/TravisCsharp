fbcdaeabfcb13482630e0bbf9f2550567ae6f792
Paul Moore <pdmoore@gmail.com>
2018-09-09 15:00:15 -0700

    initial commit

---
+++ Travis/Program.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+namespace Travis {
+    class Program {
+        static void Main(string[] args) {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ TravisTests/SampleTest.cs
@@ -0,0 +1,14 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace TravisTests {
+    
+    [TestClass]
+    public class SampleTest {
+        
+        [TestMethod]
+        public void ExampleTest() {
+            Assert.IsTrue(true);
+//            Assert.Fail("force failure to see CI fail");
+        }
+    }
+}
